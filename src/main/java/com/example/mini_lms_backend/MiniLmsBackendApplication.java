4e377aa55e6494bb9f153a720c79070bac19f0a3
Sudharaka-Sanjeewa <Sudharaka.Sanjeewa@gmail.com>
2025-06-11 11:15:28 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/mini_lms_backend/MiniLmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_lms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLmsBackendApplication.class, args);
+	}
+
+}
