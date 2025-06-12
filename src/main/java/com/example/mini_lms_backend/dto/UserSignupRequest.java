8f653bdeb16c2b6265d3ed0d9de86c5ba390db55
Sudharaka-Sanjeewa <Sudharaka.Sanjeewa@gmail.com>
2025-06-12 10:18:07 +0530

    feat: Add DTOs for signup/login with validation annotations

---
+++ src/main/java/com/example/mini_lms_backend/dto/UserSignupRequest.java
@@ -0,0 +1,19 @@
+package com.example.mini_lms_backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UserSignupRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
